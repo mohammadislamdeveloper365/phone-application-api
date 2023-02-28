44bca0268b862188718c74734a61e1d91e8a0677
SAIKAT <mohammadislam.snhu@gmail.com>
2023-02-28 10:48:46 +0400

    feature:add modal and keyboard listener

--- js/index.js
+++ js/index.js
@@ -1,66 +1,117 @@
-function loadPhones(searchValue) {
+function loadPhones(searchValue,dataLimit) {
     let url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
     fetch(url)
     .then(response => response.json())
-    .then(data=>displayPhones(data.data))
+    .then(data=>displayPhones(data.data,dataLimit))
 }
 
-loadPhones('iphone');
-addClickListener("phone-store-search-btn",searchClickHandler);
-
-function displayPhones(apiResponse) {
+loadPhones('iphone',10);
+addClickListener("phone-store-search-btn",searchHandler);
+getElement('phone-store-search-input').addEventListener('keydown',()=>{
+    if(event.key === "Enter" ) {
+        searchHandler();
+    }
+})
+function displayPhones(apiResponse,dataLimit) {
     let container = getElement("phone-store-container");
     let ele = getElement('error-message');
-    let searchAllBtn = getElement('phone-store-all-search-btn');
     toggleBodySpinner(true);
     addElementText(container,'',false);
     ele.classList.add('d-none');
 
     console.log(apiResponse)
     if(apiResponse.length) {
-        if(apiResponse.length > 10) {
+        if(apiResponse.length > 10 && dataLimit === 10) {
+            let searchAllBtn = getElement('phone-store-all-search-btn');
             apiResponse = apiResponse.slice(0,10);
-            searchAllBtn.classList.remove('d-none')
+            searchAllBtn.classList.remove('d-none');
+            console.log(searchAllBtn)
+            addClickListener('phone-store-all-search-btn',searchAllClickHandler);
         }
 
         for(phone of apiResponse) {
-            let phoneCard = createElement('div','col-lg-3','col-md-5','mr-1','mb-2','d-flex','flex-column','align-items-center');
+            let phoneCard = createElement('div','col-lg-3','col-md-5','mr-1','mb-5','d-flex','flex-column','align-items-center');
             let phoneName = createElement('p');
             let phoneImageContainer = createElement('div');
             let phoneImage = createElement('img','img-fluid');
+            let phoneModalContainer = createElement('div');
+            phoneImageContainer.classList.add('mb-1');
+            phoneModalContainer.classList.add('mb-3');
+            phoneModalContainer.innerHTML = `
+            <!-- Button to Open the Modal -->
+            <button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#myModal">
+              Phone Details
+            </button>
+            
+            <!-- The Modal -->
+            <div class="modal" id="myModal">
+              <div class="modal-dialog">
+                <div class="modal-content">
+            
+                  <!-- Modal Header -->
+                  <div class="modal-header">
+                    <h4 class="modal-title">Brand Name: ${phone.brand}</h4>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                  </div>
+            
+                  <!-- Modal body -->
+                  <div class="modal-body">
+                    Phone Model: ${phone.phone_name}
+                    <br/>
+                    image link: ${phone.image}
+                  </div>
+            
+                  <!-- Modal footer -->
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                  </div>
+            
+                </div>
+              </div>
+            </div>
+            `
             phoneImage.src = phone.image;
             addElementText(phoneName,phone.phone_name,false);
             phoneImageContainer.append(phoneImage);
-            phoneCard.append(phoneName,phoneImageContainer)
+            phoneCard.append(phoneName,phoneImageContainer,phoneModalContainer)
             container.append(phoneCard);
         
         }
 
         toggleBodySpinner(false);
         toggleButtonSpinner(false);
+        toggleAllButtonSpinner(false);
     }
 
     else {
         toggleBodySpinner(false);
         toggleButtonSpinner(false);
+        toggleAllButtonSpinner(false);
         ele.classList.remove('d-none');
         
     }
     
 }
 
-
-
-function searchClickHandler() {
+function processSearch(dataLimit) {
     let inputValue = getElementValue('phone-store-search-input',true);
-    let searchAllBtn = getElement('phone-store-all-search-btn');
-    searchAllBtn.classList.add('d-none');
     inputValue = inputValue.trim();
     console.log(inputValue);
-    toggleButtonSpinner(true);
-    loadPhones(inputValue)
+    loadPhones(inputValue,dataLimit)
 }
 
+function searchHandler() {
+    let searchAllBtn = getElement('phone-store-all-search-btn');
+    searchAllBtn.classList.add('d-none');
+    toggleButtonSpinner(true);
+    processSearch(10);
+}
+
+
+function searchAllClickHandler() {
+    toggleAllButtonSpinner(true);
+    processSearch();
+}
 function createElement(tagName,...classList) {
     let ele = document.createElement(tagName);
     if(classList) {
@@ -119,15 +170,15 @@ function toggleButtonSpinner(isLoading) {
     }
 }
 
-// function toggleAllButtonSpinner(isLoading) {
-//     if(isLoading) {
-//         getElement('loading-all-text').classList.remove('d-none');
-//         getElement('search-all-text').classList.add('d-none');
+function toggleAllButtonSpinner(isLoading) {
+    if(isLoading) {
+        getElement('loading-all-text').classList.remove('d-none');
+        getElement('search-all-text').classList.add('d-none');
         
-//     }
+    }
     
-//     else {
-//         getElement('loading-all-text').classList.add('d-none');
-//         getElement('search-all-text').classList.remove('d-none');
-//     }
-// }
+    else {
+        getElement('loading-all-text').classList.add('d-none');
+        getElement('search-all-text').classList.remove('d-none');
+    }
+}
